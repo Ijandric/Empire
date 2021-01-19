03e6d6ab768623bda5b27f5efdf6c9959787b965
Ivan Jandric <ivan.jandric@protel.net>
2021-01-19 16:10:04 +0100

    style5

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let buttons = document.querySelectorAll(".btn");
 
 buttons.forEach(e => {
@@ -20,19 +20,6 @@ info.addEventListener('click', () => {
     info.classList.toggle("active");
     info.innerText = info.innerText === '?' ? 'X' : '?'
 })
-=======
-let buttons = document.querySelectorAll(".btn");
 
-buttons.forEach(e => {
-    e.addEventListener('click', (event) => {
-        console.log("click", event)
-        let parent = event.target.parentElement;
-        if (event.target.innerText === "+") {
-            //parent.firstChild.innerText = Number(parent.firstChild.innerText) + 1;
-            parent.firstElementChild.innerText++
-        } else {
-            parent.firstElementChild.innerText--
-        }
-    });
-});
->>>>>>> c1f4be54256d65f6c2f7b93e153b22728b1b38e6
+
+
