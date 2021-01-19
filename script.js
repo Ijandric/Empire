c1f4be54256d65f6c2f7b93e153b22728b1b38e6
Ijandric <Ijandric87@gmail.com>
2021-01-19 15:21:07 +0100

    first
    first

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let buttons = document.querySelectorAll(".btn");
 
 buttons.forEach(e => {
     e.addEventListener('click', (event) => {
+        console.log("click", event)
         let parent = event.target.parentElement;
         if (event.target.innerText === "+") {
+            //parent.firstChild.innerText = Number(parent.firstChild.innerText) + 1;
             parent.firstElementChild.innerText++
         } else {
             parent.firstElementChild.innerText--
